3d3e7f3da6378aac94a072048be3f95d5a1e44e8
BlochLior <blochlior@gmail.com>
2025-07-13 09:21:33 +0300

    part 4

---
+++ backend-django/sql/001_case_insert_polls_choice_2.sql
@@ -0,0 +1,6 @@
+-- Add two choices to the second question
+-- Added: 12/07/2025
+
+INSERT INTO polls_choice (choice_text, votes, question_id) VALUES 
+    ('Pretty good', 0, 2),
+    ('Been better', 0, 2);
